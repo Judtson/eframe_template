edaf54a621fb39ce9b4fb62896d3ea8211d2f90c
Judtson <87301382+Judtson@users.noreply.github.com>
2023-10-15 20:04:43 +0200

    Set dark style as default

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use eframe::egui::{Style, Visuals};
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
@@ -14,7 +16,14 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| {
+            let style = Style {
+                visuals: Visuals::dark(),
+                ..Style::default()
+            };
+            cc.egui_ctx.set_style(style);
+            Box::new(eframe_template::TemplateApp::new(cc))
+        }),
     )
 }
 
